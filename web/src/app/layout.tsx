79e86175310b0e8e3681674ad6f2d5d886dcba64
mantasmatij <mantas.matijosaitis@helmes.com>
2025-11-27 13:08:45 +0200

    Move Atkinson Hyperlegible font files to web/public/fonts and update import paths in layout.tsx for Vercel deployment

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -14,22 +14,22 @@ export const metadata = {
 const atkinson = localFont({
   src: [
     {
-      path: '../../../stuff/Atkinson-Hyperlegible-Font-Print-and-Web-2020-0514/Web Fonts/WOFF2/Atkinson-Hyperlegible-Regular-102a.woff2',
+      path: '/fonts/Atkinson-Hyperlegible-Regular-102a.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../../../stuff/Atkinson-Hyperlegible-Font-Print-and-Web-2020-0514/Web Fonts/WOFF2/Atkinson-Hyperlegible-Italic-102a.woff2',
+      path: '/fonts/Atkinson-Hyperlegible-Italic-102a.woff2',
       weight: '400',
       style: 'italic',
     },
     {
-      path: '../../../stuff/Atkinson-Hyperlegible-Font-Print-and-Web-2020-0514/Web Fonts/WOFF2/Atkinson-Hyperlegible-Bold-102a.woff2',
+      path: '/fonts/Atkinson-Hyperlegible-Bold-102a.woff2',
       weight: '700',
       style: 'normal',
     },
     {
-      path: '../../../stuff/Atkinson-Hyperlegible-Font-Print-and-Web-2020-0514/Web Fonts/WOFF2/Atkinson-Hyperlegible-BoldItalic-102a.woff2',
+      path: '/fonts/Atkinson-Hyperlegible-BoldItalic-102a.woff2',
       weight: '700',
       style: 'italic',
     },
